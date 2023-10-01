23acff8525bc1cfe6b1f328611c980ad8e9fafe3
Silent <zdanio95@gmail.com>
2023-10-01 20:41:53 +0200

    Initial commit

---
+++ source/VersionInfo.lua
@@ -0,0 +1,9 @@
+defines { 
+    "rsc_FullName=\"SilentPatch for Juiced (Demo)\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2023\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchJuicedDemo/releases\"",
+    "rsc_CompanyName=\"Silent\""
+}
